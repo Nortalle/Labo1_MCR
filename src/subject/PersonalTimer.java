5578e8651ca51ae21725c8beae7d766ed53ba7f5
Nortalle <vincent.guidoux1@heig-vd.ch>
2018-03-13 21:36:11 +0100

    O K HOu2

--- src/subject/PersonalTimer.java
+++ src/subject/PersonalTimer.java
@@ -2,7 +2,7 @@
  -----------------------------------------------------------------------------------
  Laboratoire : 1
  Fichier     : PersonalTimer.java
- Auteur(s)   : Samuel Mayoer et Vincent Guidoux
+ Auteur(s)   : Samuel Mayor et Vincent Guidoux
  Date        : 04.03.2018
 
  But         : Concret Subject of the Observable model (GoF)
@@ -23,8 +23,13 @@ public class PersonalTimer implements Subject {
     private int time;
     private boolean paused = true;
     
+    /**
+     * Initialise the Iimer, and set the seconds time
+     */
     private void scheduleTimer() {
         timer = new Timer();
+        // Every schedule of the timer, one second is added and all the observers 
+        // are notified
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
@@ -34,47 +39,71 @@ public class PersonalTimer implements Subject {
         }, 10, 10);
     }
     
+    /**
+     * Initialisation
+     */
     public PersonalTimer() {
         observers = new LinkedList<>();
         
         time = 0;
     }
     
+    /**
+     * @return the number of hours passed
+     */
     public int hours() {
         return time / 3600;
     }
     
+    /**
+     * @return the number of minutes passed
+     */
     public int minutes() {
         return (time / 60) % 60;
     }
     
+    /**
+     * @return the number of seconds passed
+     */
     public int seconds() {
         return time % 60;
     }
     
+    /**
+     * @return if the clock is paused
+     */
     public boolean paused() {
         return paused;
     }
     
+    /**
+     * resets the clock to 0 and notify the observers
+     */
     public void reset() {
         time = 0;
         notifyObservers();
     }
     
+    /**
+     * Pauses the clock if not already paused
+     */
     public void pause() {
         if (!paused) {
             timer.cancel();
             paused = true;
         }
-        
     }
     
+    /**
+     * Starts the clock if paused
+     */
     public void start() {
         if (paused) {
             paused = false;
             scheduleTimer();
         }
     }
+    
     
     @Override
     public void attach(Observer o) {
